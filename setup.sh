f16537faab9e8524dac5a3fb40daa93bc8a37f3e
Aakanksha Gubbala <aakanksha.gubbala@gmail.com>
2021-12-23 21:49:00 +0530

    ~

--- setup.sh
+++ setup.sh
@@ -1,8 +1,12 @@
 mkdir -p ~/.streamlit/
-echo "\
-[server]\n\
-headless = true\n\
-port = $PORT\n\
-enableCORS = false\n\
-\n\
+echo "[theme]
+primaryColor='#0013f7'
+backgroundColor='#ffffff'
+secondaryBackgroundColor='#f9ef91'
+textColor='#000000'
+font='monospace'
+[server]
+headless = true
+port = $PORT
+enableCORS = false
 " > ~/.streamlit/config.toml
