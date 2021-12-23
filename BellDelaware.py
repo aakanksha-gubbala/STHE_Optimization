8bc9a1fee67c1501cd76d2e47e1b6309ff17052f
Aakanksha Gubbala <aakanksha.gubbala@gmail.com>
2021-12-23 21:33:56 +0530

    ~

--- BellDelaware.py
+++ BellDelaware.py
@@ -2,14 +2,14 @@ import streamlit as st
 import numpy as np
 import lxml
 import ref
-# import STHE_BD
+import theory
 
 np.seterr(divide='ignore', invalid='ignore')
 
-PAGES = {"Home": ref}
+PAGES = {"Home": ref, "Theory": theory}
 
 st.sidebar.title("Navigation")
-selection = st.sidebar.radio("Go to", ["Home"])
+selection = st.sidebar.radio("Go to", ["Home", "Theory"])
 
 with st.spinner(f'Loading {selection} ...'):
     PAGES[selection].main()
@@ -17,5 +17,5 @@ with st.spinner(f'Loading {selection} ...'):
 st.sidebar.title("About")
 st.sidebar.info("Heat exchanger design & optimization")
 st.sidebar.info("Heat transfer calculations by Bell-Delaware Method")
-st.sidebar.info("""Here's the [link](https://github.com/aakanksha-gubbala/) to the source code""")
+st.sidebar.info("""Here's the [link](https://github.com/aakanksha-gubbala/STHE_Optimization) to the source code""")
 
---
+++ theory.py
@@ -0,0 +1,28 @@
+import streamlit as st
+
+
+def main():
+    st.title("STHE Classes")
+    st.subheader("Problem Solving Approach")
+    st.write("Objective : Minimize total cost (fabrication + operating costs) of STHE")
+    st.write("Constraints : ")
+    st.write(r"1. $\Delta P_s < \Delta P_{s, permissible}$")
+    st.write(r"2. $\Delta P_t < \Delta P_{t, permissible}$")
+    st.write(r"3. $A_{calc} < A_{required}$")
+    st.write("Subject to variables : ")
+    st.write(r"1. Tube layout = [Traingular 30$^o$, Rotated square 45$^o$, Square 90$^o$]")
+    st.write(r"2. Number of tube passes = [1, 2, 4]")
+    st.write(r"3. Baffle cut = 15% to 25%")
+    st.write(r"4. Length of tube = 2.5 to 6 m")
+    st.write(r"5. Outer diameter of tube = 0.75 in, 1 in, 1.25 in, 1.5 in, 2 in")
+    st.write(r"6. Shell-baffle clearance = 5 to 25 mm")
+    st.write(r"7. Tube-baffle clearance = 5 to 25 mm")
+    st.write("Assumptions")
+    st.write(r"1. Tube Thickness is 16 BWG")
+    st.write(r"2. Baffle spacing = 0.5D$_s$")
+    st.write(r"3. Constant specific heat capacity in the temperature range")
+    st.write("References")
+    st.write("1) Fettaka, S., Thibault, J., & Gupta, Y. (2013). "
+             "Design of shell-and-tube heat exchangers using multiobjective optimization."
+             " International Journal of Heat and Mass Transfer, 60, 343–354.")
+    st.write("2) Hewitt, Geoffrey F., Theodore R. Bott, and G. L. Shires (1994) Process Heat Transfer. CRC Press.")
